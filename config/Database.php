dc3328a728a6d2fbf2f79a0f2253e246debc356e
Samuel Heinrich <shazamuel89@gmail.com>
2025-03-03 21:21:21 -0600

    Changed db support from mysql to postgresql

--- config/Database.php
+++ config/Database.php
@@ -2,22 +2,25 @@
     class Database {
         // DB Params
         private $host = 'localhost';
-        private $db_name = 'myblog';
-        private $username = 'root';
-        private $password = '';
+        private $port = '5432';
+        private $db_name = 'quotesdb';
+        private $username = 'postgres';
+        private $password = 'postgres';
         private $conn;
 
         // DB Connect
         public function connect() {
             $this->conn = null;
+            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
             try {
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);  // Set up conn as a PDO object
+                $this->conn = new PDO($dsn, $this->username, $this->password);  // Set up conn as a PDO object
+
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                                               // Set conn's attributes to allow errors to be shown
             } catch (PDOException $e) {
+                // Echo for the tutorial, but log the error for production
                 echo 'Connection Error: ' . $e->getMessage();
             }
-
             return $this->conn;
         }
     }
